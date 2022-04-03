4e01151a8966d6f7d596c5c1911e089d6fb3f08a
Daniel Winther <danielwintherjensen@gmail.com>
2022-04-03 23:06:30 +0200

    Google api check if event exists

--- scraper_notifier/pipelines.py
+++ scraper_notifier/pipelines.py
@@ -4,9 +4,10 @@
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
+from ast import If, Try
 import datetime
 import os.path
-import json
+from tkinter import E
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
@@ -20,25 +21,30 @@ class ScraperNotifierPipeline(object):
         creds = Credentials.from_authorized_user_file(os.path.dirname(os.path.abspath(__file__)) + '/token.json', SCOPES)
         service = build('calendar', 'v3', credentials=creds)
 
-        event = {
-            'summary': item['group'],
-            'location': 'Degnestavnen, 2400 København NV',
-            'start': {
-                'dateTime': item['reserved_at'].isoformat(),
-                'timeZone': 'Europe/Copenhagen',
-            },
-            'end': {
-                'dateTime': item['reserved_at'].isoformat(),
-                'timeZone': 'Europe/Copenhagen',
-            },
-            'reminders': {
-                'useDefault': False,
-                'overrides': [
-                    {'method': 'popup', 'minutes': 30},
-                    {'method': 'popup', 'minutes': 1440}
-                ],
-            },
-        }
-        event = service.events().insert(calendarId='primary', body=event).execute()
+        try:
+            eventExist = service.events().get(calendarId='primary', eventId='vasketid' + item['reserved_at'].strftime('%Y%m%d%H%M')).execute()
+        except:
+            eventExist = None
+        if eventExist is None:
+            service.events().insert(calendarId='primary', body={
+                'id': 'vasketid' + item['reserved_at'].strftime('%Y%m%d%H%M'),
+                'summary': item['group'],
+                'location': 'Degnestavnen, 2400 København NV',
+                'start': {
+                    'dateTime': item['reserved_at'].isoformat(),
+                    'timeZone': 'Europe/Copenhagen',
+                },
+                'end': {
+                    'dateTime': item['reserved_at'].isoformat(),
+                    'timeZone': 'Europe/Copenhagen',
+                },
+                'reminders': {
+                    'useDefault': False,
+                    'overrides': [
+                        {'method': 'popup', 'minutes': 30},
+                        {'method': 'popup', 'minutes': 1440}
+                    ],
+                },
+            }).execute()
 
         return item
